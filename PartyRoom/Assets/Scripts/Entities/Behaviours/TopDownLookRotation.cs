2dddb2f5c8207dbea4c062964d6fa94d6a6fdaa8
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-10 17:38:18 +0900

    [feat] stay 애니메이션에서 walk 애니메이션으로 자연스럽게 전환

--- PartyRoom/Assets/Scripts/Entities/Behaviours/TopDownLookRotation.cs
+++ PartyRoom/Assets/Scripts/Entities/Behaviours/TopDownLookRotation.cs
@@ -7,6 +7,8 @@ public class TopDownLookRotation : MonoBehaviour
 
     private TopDownController controller;
 
+    private bool IsWalk = false;
+
     private void Awake()
     {
         controller = GetComponent<TopDownController>();
@@ -19,10 +21,20 @@ public class TopDownLookRotation : MonoBehaviour
         // 마우스의 위치가 들어오는 OnLookEvent에 등록. 캐릭터 이미지 방향전환에 사용.
         controller.OnLookEvent += OnAim;
     }
-    
+
     public void OnWalk(Vector2 playerMovement)
     {
-
+        //우선은 움직임만 감지하도록 구현.
+        if (playerMovement == Vector2.zero)
+        {
+            IsWalk = false;
+            characterAnimator.SetBool("IsWalk", false);
+        }
+    else
+        {
+            IsWalk = true;
+            characterAnimator.SetBool("IsWalk", true);
+        }
     }
 
     public void OnAim(Vector2 newAimDirection)
@@ -37,8 +49,41 @@ public class TopDownLookRotation : MonoBehaviour
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         int playerVisionRange = GetMouseDirection(rotZ);
-        ChangeAnimationStay(playerVisionRange);
 
+        if (IsWalk)
+        {
+            ChangeAnimationWalk(playerVisionRange);
+        }
+        else
+        {
+            ChangeAnimationStay(playerVisionRange);
+        }
+    }
+
+    private void ChangeAnimationWalk(int visionRange)
+    {
+        AnimatorStateInfo stateInfo = characterAnimator.GetCurrentAnimatorStateInfo(0);
+        float syncTime = stateInfo.normalizedTime % 1;
+
+        switch (visionRange)
+        {
+            case 0:
+                characterAnimator.Play("PlayerWalkUp", 0, syncTime);
+                break;
+            case 1:
+                characterAnimator.Play("PlayerWalkDown", 0, syncTime);
+                break;
+            case 2:
+                characterRenderer.flipX = false;
+                characterAnimator.Play("PlayerWalkSide", 0, syncTime); //왼쪽
+                break;
+            case 3:
+                characterRenderer.flipX = true;
+                characterAnimator.Play("PlayerWalkSide", 0, syncTime); //오른쪽
+                break;
+            default:
+                break;
+        }
     }
 
     private void ChangeAnimationStay(int visionRange)
