83325b7675e8303992faceaf3ab95b3fd87f9887
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-14 07:21:22 +0900

    [feat] participant 목록

--- PartyRoom/Assets/Scripts/Interacts/ParticipantListText.cs
+++ PartyRoom/Assets/Scripts/Interacts/ParticipantListText.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ParticipantListText : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject SidePanel;
+    public Text participant;
+
+    private Animator sideAnim;
+
     void Start()
     {
-        
+         sideAnim = GetComponent<Animator>();
+
+        GameObject[] NPCList = GameObject.FindGameObjectsWithTag("NPC");
+
+        string playerName = GameManager.instance.savePlayerData.characterName;
+        participant.text += (playerName + "\n");
+
+        foreach (GameObject NPC in NPCList)
+        {
+            string name = NPC.GetComponent<NPCDataHandler>().GetNPCName();
+
+            participant.text += (name + "\n");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnSidePanelClick()
     {
-        
+        if (sideAnim.GetBool("IsClicked"))
+        {
+            sideAnim.SetBool("IsClicked", false);
+        }
+        else
+        {
+            sideAnim.SetBool("IsClicked", true);
+        }
     }
 }
